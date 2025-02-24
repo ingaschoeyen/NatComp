ca753148841a2347ce24ef91dfc9d5390eb5bb6a
Jaroměl Tuzerád <jaromel.tuzerad@gmail.com>
2025-02-24 13:41:02 +0100

    handle line-length non-divisble by chunk-size

--- negative-selection/equalise_overlap.py
+++ negative-selection/equalise_overlap.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # sys args: data.in n overlap data.out labels.out
 
 n = int(sys.argv[2])
-overlap = int(sys.argv[3])  
+overlap = int(sys.argv[3])
 
 def chunk_sequence(sequence, chunk_size, overlap):
     chunks = []
@@ -13,13 +13,15 @@ def chunk_sequence(sequence, chunk_size, overlap):
     while start + chunk_size <= len(sequence):
         chunks.append(sequence[start : start + chunk_size])
         start += (chunk_size - overlap)
+    missing = chunk_size - len(sequence) + start
+    chunks.append(sequence[start:] + sequence[:missing])
     return chunks
 
 with open(sys.argv[1], 'r') as data_in, open(sys.argv[4], 'w') as data_out, open(sys.argv[5], 'w') as labels:
-    
+
     # Read test sequences and labels
     test_sequences = [line.strip() for line in data_in]
-    
+
     # Process each sequence
     for i, seq in enumerate(test_sequences):
         chunks = chunk_sequence(seq, n, overlap)
