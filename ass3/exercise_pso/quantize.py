6f0fd66d71740f975bbfafe2ae8190671578827d
Jaroměl Tuzerád <jaromel.tuzerad@gmail.com>
2025-02-28 17:31:00 +0100

    fix outputPath

--- ass3/exercise_pso/quantize.py
+++ ass3/exercise_pso/quantize.py
@@ -7,20 +7,20 @@ from matplotlib import pyplot as plt
 
 # argv: input.png output.png
 
-DEF_PALLETE = np.array([
+PALLETE_DEF = np.array([
     (175, 198, 114),
     (83, 20, 14),
     (185, 49, 40),
     (124, 148, 71)
 ])
 
-def quantize(img, pallete = DEF_PALLETE):
+def quantize(img, pallete = PALLETE_DEF):
     height, width, _ = np.array(img).shape
     img = np.array(img.getdata())
 
     distances = np.zeros((img.shape[0], len(pallete)))
     for i in range(len(pallete)):
-        euc_dist = np.apply_along_axis(np.linalg.norm, axis=1, arr=img[:, :3] - DEF_PALLETE[i])
+        euc_dist = np.apply_along_axis(np.linalg.norm, axis=1, arr=img[:, :3] - PALLETE_DEF[i])
         distances[:, i] = euc_dist
 
     closest_colour = np.argmin(distances, axis=1)
@@ -34,7 +34,7 @@ def quantize(img, pallete = DEF_PALLETE):
 
 def saveImg(img, outputPath):
     img = Image.fromarray(img.astype(np.uint8)).transpose(Image.Transpose.FLIP_LEFT_RIGHT).transpose(Image.Transpose.ROTATE_90)
-    img.save("output.png")
+    img.save(outputPath)
 
 if __name__ == '__main__':
     saveImg(quantize(Image.open(sys.argv[1])), sys.argv[2])
