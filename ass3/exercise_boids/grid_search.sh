428b6e67806df207c710949ce2408ba3bd43ff25
Jaroměl Tuzerád <jaromel.tuzerad@gmail.com>
2025-03-31 18:41:32 +0200

    ass3: paralelize grid search script

--- ass3/exercise_boids/grid_search.sh
+++ ass3/exercise_boids/grid_search.sh
@@ -3,17 +3,25 @@
 N=100
 max=9
 inner=10
-outer=50
-for i in $(seq 1 $max)
+outer=100
+
+# select the range of params to simulate asynchronyously
+declare -a a_params=$(seq 1 1)
+declare -a b_params=$(seq 1 1)
+declare -a c_params=$(seq 1 1)
+
+for i in $a_params;
 do
-    for j in $(seq 1 $max)
+    for j in $b_params;
     do
-        for k in $(seq 1 $max)
+        for k in $c_params;
         do
             echo "Running simulation with N=$N, cohesion=$i, separation=$j, alignment=$k"
-            node boids_grid.mjs $N $i $j $k $max $inner $outer  
-        done 
+            node boids_grid.mjs $N $i $j $k $max $inner $outer &
+        done
     done
 done
-# usage ./grid_search.sh 
-python grid_search.py $N $max $inner $outer
+# usage ./grid_search.sh
+wait
+# python3 grid_search.py $N $max $inner $outer
+
