d6024e3d5716336a0dd839f9bc57390d41afd783
inga420 <60630082+inga420@users.noreply.github.com>
2025-05-17 09:27:33 +0200

    added base structure voter

---
+++ FP/sim_engine.py
@@ -0,0 +1,13 @@
+import Population
+import Government
+
+
+class Simulation():
+
+    def __init__():
+        pass
+
+
+    def do_an_election_run():
+        population = Population()
+        government = Government()
---
+++ FP/voter_models.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Voter():
+
+    def __init__():
+        # initialise with random position and strategy
+        pass
+
+class Population(Voter):
+
+    def __init__(population_size=100):
+        self.N = population_size
+        self.initialisePop()
+        self.pop_div = 0 # divergence of pop opinion spread
+        self.pop_mean = 0 # mean of pop opinion
+
+    def initialisePop():
+        self.pop = []
+        for n in range(self.N):
+            self.pop.append(super.__init__)
+        self.pop_div = 
+        self.pop_mean = 
